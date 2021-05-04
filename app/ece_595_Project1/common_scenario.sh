1c94b14de29b31e0f58e41c8fedf0e3a2113f976
George Fragkos <georgios.fragkos@cern.ch>
2021-05-04 15:26:05 -0600

    Common scenario updates

--- app/ece_595_Project1/common_scenario.sh
+++ app/ece_595_Project1/common_scenario.sh
@@ -2,6 +2,7 @@
 
 
 # 2 slices can be mapped to 2 virtual queues!
+# Creating 2 virtual queues in Router 1.
 echo ' ---------------------------------------------- '
 echo '*** Network Slicing: Creating 2 slices of 5 Mbps each ...'
 echo 'Router1:'
@@ -15,6 +16,8 @@ queues:234=@2q -- \
 
 echo ' '
 
+# 2 slices can be mapped to 2 virtual queues!
+# Creating 2 virtual queues in Router 2.
 echo 'Router2:'
 sudo ovs-vsctl set port r2-eth1 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
@@ -27,8 +30,11 @@ queues:234=@2q -- \
 
 echo '*** End of Creating the Slices ...'
 echo ' ---------------------------------------------- '
+
+# Mapping the r1 virtual queues to hosts: (h1, h4) --> queue123, (h2, h5) --> queue234
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.4,idle_timeout=0,actions=set_queue:123,normal
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.5,idle_timeout=0,actions=set_queue:234,normal
+# Making sure that only these hosts can communicate with each other: (h1, h3), (h2, h4)
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
@@ -39,8 +45,10 @@ sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.4,idl
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow r1 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
 
+# Mapping the r2 virtual queues to hosts: (h1, h4) --> queue123, (h2, h5) --> queue234
 sudo ovs-ofctl add-flow r2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.1,idle_timeout=0,actions=set_queue:123,normal
 sudo ovs-ofctl add-flow r2 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.2,idle_timeout=0,actions=set_queue:234,normal
+# Making sure that only these hosts can communicate with each other: (h1, h3), (h2, h4)
 sudo ovs-ofctl add-flow r2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow r2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow r2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
